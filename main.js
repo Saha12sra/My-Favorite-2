d14b3f47e251e0bd996b9310c10264236705cdc6
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2020-11-08 16:19:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function welcomefav() {
+    document.getElementById("s1").style.display = "none";
+    document.getElementById("s2").style.display = "block";
+}
+
+function favorite() {
+    document.getElementById("s2").style.display = "none";
+
+    document.getElementById("s3").style.display = "block";
+
+    document.getElementById("s4").style.display = "block";
+}
+
+function hawaii() {
+    document.getElementById("s3").style.display = "none";
+    document.getElementById("s4").style.display = "none";
+    document.getElementById("s5").style.display = "block";
+}
+
+function back2b() {
+    document.getElementById("s5").style.display = "none";
+    document.getElementById("s3").style.display = "block";
+    document.getElementById("s4").style.display = "block";
+}
